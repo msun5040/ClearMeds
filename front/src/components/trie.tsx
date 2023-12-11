4d5d6e3bca0c8d371d36bba2659a0fb963a65e08
elaine-wangg <elaine_wang1@brown.edu>
2023-12-11 17:26:14 -0500

    feat: trie building and suggestions tsx and css

---
+++ front/src/components/trie.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+interface TrieNode {
+  children: { [key: string]: TrieNode };
+  isEndOfWord: boolean;
+}
+
+const buildTrie = (words: string[]): TrieNode => {
+  const root: TrieNode = { children: {}, isEndOfWord: false };
+
+  for (const word of words) {
+    let node = root;
+    const lowercasedWord = word.toLowerCase(); // Convert the word to lowercase
+    for (const char of lowercasedWord) {
+      if (!node.children[char]) {
+        node.children[char] = { children: {}, isEndOfWord: false };
+      }
+      node = node.children[char];
+    }
+    node.isEndOfWord = true;
+  }
+  return root;
+};
+
+const findSuggestions = (trie: TrieNode, prefix: string): string[] => {
+  let node = trie;
+  for (const char of prefix) {
+    if (!node.children[char]) {
+      return [];
+    }
+    node = node.children[char];
+  }
+
+  const suggestions: string[] = [];
+  collectSuggestions(node, prefix, suggestions);
+
+  return suggestions;
+};
+
+const collectSuggestions = (
+  node: TrieNode,
+  currentPrefix: string,
+  suggestions: string[]
+): void => {
+  if (node.isEndOfWord) {
+    suggestions.push(currentPrefix);
+  }
+
+  for (const char in node.children) {
+    collectSuggestions(node.children[char], currentPrefix + char, suggestions);
+  }
+};
+
+export { buildTrie, findSuggestions, collectSuggestions };
