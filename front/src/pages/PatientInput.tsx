56fe2e2f8846c03fe172bd5029ca88563ba86292
“msun5040 <michael_h_sun@brown.edu>
2023-12-12 13:19:29 -0500

    fix: flexbox for patient input

--- front/src/pages/PatientInput.tsx
+++ front/src/pages/PatientInput.tsx
@@ -104,112 +104,112 @@ const PatientInput: React.FC = () => {
       <div className="form-text">
         <div className="text-line">Welcome Patients</div>
       </div>
-      <div className="form-container">
-        <div className="form">
-          <div className="suggestions-container">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Active Ingredients"
-              value={activeIngredients}
-              onChange={(e) =>
-                handleInputChange(
-                  e,
-                  setActiveIngredients,
-                  setActiveIngredientsSuggestions
-                )
-              }
-              ref={inputRef}
-            />
-            {activeIngredientsSuggestions.length > 0 && (
-              <ul>
-                {activeIngredientsSuggestions.map((suggestion) => (
-                  <li
-                    key={suggestion}
-                    onClick={() =>
-                      handleSuggestionClick(
-                        suggestion,
-                        setActiveIngredients,
-                        setActiveIngredientsSuggestions
-                      )
-                    }
-                  >
-                    {suggestion}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-          <div className="suggestions-container">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Allergies"
-              value={allergies}
-              onChange={(e) =>
-                handleInputChange(e, setAllergies, setAllergiesSuggestions)
-              }
-            />
-            {allergiesSuggestions.length > 0 && (
-              <ul>
-                {allergiesSuggestions.map((suggestion) => (
-                  <li
-                    key={suggestion}
-                    onClick={() =>
-                      handleSuggestionClick(
-                        suggestion,
-                        setAllergies,
-                        setAllergiesSuggestions
-                      )
-                    }
-                  >
-                    {suggestion}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-          <div className="suggestions-container">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Additional Fields"
-              value={additionalFields}
-              onChange={(e) =>
-                handleInputChange(
-                  e,
-                  setAdditionalFields,
-                  setAdditionalFieldsSuggestions
-                )
-              }
-            />
-            {additionalFieldsSuggestions.length > 0 && (
-              <ul>
-                {additionalFieldsSuggestions.map((suggestion) => (
-                  <li
-                    key={suggestion}
-                    onClick={() =>
-                      handleSuggestionClick(
-                        suggestion,
-                        setAdditionalFields,
-                        setAdditionalFieldsSuggestions
-                      )
-                    }
-                  >
-                    {suggestion}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-          <div className="input-button-container">
-            <button className="form-button" onClick={handleClickBack}>
-              Back
-            </button>
-            <button className="form-button" onClick={handleSubmit}>
-              Submit
-            </button>
-            <div className="input-message">Yo</div>
+      <div className="form">
+        <div className="suggestions-container">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Active Ingredients"
+            value={activeIngredients}
+            onChange={(e) =>
+              handleInputChange(
+                e,
+                setActiveIngredients,
+                setActiveIngredientsSuggestions
+              )
+            }
+            ref={inputRef}
+          />
+          {activeIngredientsSuggestions.length > 0 && (
+            <ul>
+              {activeIngredientsSuggestions.map((suggestion) => (
+                <li
+                  key={suggestion}
+                  onClick={() =>
+                    handleSuggestionClick(
+                      suggestion,
+                      setActiveIngredients,
+                      setActiveIngredientsSuggestions
+                    )
+                  }
+                >
+                  {suggestion}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className="suggestions-container">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Allergies"
+            value={allergies}
+            onChange={(e) =>
+              handleInputChange(e, setAllergies, setAllergiesSuggestions)
+            }
+          />
+          {allergiesSuggestions.length > 0 && (
+            <ul>
+              {allergiesSuggestions.map((suggestion) => (
+                <li
+                  key={suggestion}
+                  onClick={() =>
+                    handleSuggestionClick(
+                      suggestion,
+                      setAllergies,
+                      setAllergiesSuggestions
+                    )
+                  }
+                >
+                  {suggestion}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className="suggestions-container">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Additional Fields"
+            value={additionalFields}
+            onChange={(e) =>
+              handleInputChange(
+                e,
+                setAdditionalFields,
+                setAdditionalFieldsSuggestions
+              )
+            }
+          />
+          {additionalFieldsSuggestions.length > 0 && (
+            <ul>
+              {additionalFieldsSuggestions.map((suggestion) => (
+                <li
+                  key={suggestion}
+                  onClick={() =>
+                    handleSuggestionClick(
+                      suggestion,
+                      setAdditionalFields,
+                      setAdditionalFieldsSuggestions
+                    )
+                  }
+                >
+                  {suggestion}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className="input-button-container">
+          <button className="form-button" onClick={handleClickBack}>
+            Back
+          </button>
+          <button className="form-button" onClick={handleSubmit}>
+            Submit
+          </button>
+          <div className="input-message">
+                  Yo
           </div>
         </div>
       </div>
