480c5328246dc6ebdefcd66452a2f3c822d6b48f
“msun5040 <michael_h_sun@brown.edu>
2023-12-13 01:00:22 -0500

    feat: patient output UI

---
+++ front/src/components/ResultBox.tsx
@@ -0,0 +1,29 @@
+interface PatientOutputProps {
+  drugBrand: string;
+  genericName: string;
+  activeIngredients: string;
+  uses: string;
+  manufacturers: string;
+  marketingStatus: string;
+}
+
+export default function ResultBox(props: PatientOutputProps) {
+
+  return (
+    <div className="result-box">
+      <div className="result-entry-title">{props.drugBrand}</div>
+      <div className="result-entry-subtitle">{props.genericName}</div>
+      <div className="result-entry-text-header">Active Ingredients:</div>
+      <div className="result-entry-text-body">{props.activeIngredients}</div>
+      <div className="result-entry-text-header">Uses:</div>
+      <div className="result-entry-text-body">{props.uses}</div>
+      <div className="result-entry-text-header">Manufacturers:</div>
+      <div className="result-entry-text-body">{props.manufacturers}</div>
+      <div className="result-entry-text-header">Marketing Status:</div>
+      <div className="result-entry-text-body">{props.marketingStatus}</div>
+      <div className="patient-output-learnmore-container">
+        <button className="patient-output-learnmore">Learn More</button>
+      </div>
+    </div>
+  );
+}
--- front/src/pages/PatientOutput.tsx
+++ front/src/pages/PatientOutput.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import ResultBox from "../components/ResultBox";
 
 const PatientOutput: React.FC = () => {
   const navigate = useNavigate();
@@ -18,16 +19,44 @@ const PatientOutput: React.FC = () => {
 
   return (
     <div className="result-body">
-      <div className="text-line">Search Results</div>
-      <div className="result-container">
-        <div className="result-box"></div>
-        <div className="result-box"></div>
-        <div className="result-box"></div>
-        <div className="result-box"></div>
+      <div className="output-title">Search Results</div>
+      <div className="search-summary">
+        Active Ingredients: {activeIngredients}
       </div>
-      <p>Active Ingredients: {activeIngredients}</p>
-      <p>Allergies: {allergies}</p>
-      <p>Additional Fields: {additionalFields}</p>
+      <div className="search-summary">Allergies: {allergies}</div>
+      <div className="search-summary">
+        Additional Fields: {additionalFields}
+      </div>
+
+      <div className="result-container">
+        {/* Result 1 */}
+        <ResultBox
+          drugBrand = {"Drug 1"}
+          genericName = {"Aceto"}
+          activeIngredients = {"Acetomenaphin"}
+          uses = {"cures the plague"}
+          manufacturers = {"Johnson & Johnson"}
+          marketingStatus = {"Discontinued"}
+        />
+        {/* Result 2 */}
+        <div className="result-box">
+          <div className="result-entry-title">Drug Brand 1</div>
+          <div className="result-entry-subtitle">Generic name</div>
+          <div className="result-entry-text-header">Active Ingredients:</div>
+          <div className="result-entry-text-body">Ibuprofen</div>
+          <div className="result-entry-text-header">Uses:</div>
+          <div className="result-entry-text-body">anit-sick</div>
+          <div className="result-entry-text-header">Manufacturers:</div>
+          <div className="result-entry-text-body">Johnson & Johnson</div>
+          <div className="result-entry-text-header">Marketing Status:</div>
+          <div className="result-entry-text-body">
+            Discontinued aklsdjalkdjadklajdakldjalkdsjakldjaiodjaldakldjalkdsa
+          </div>
+          <div className="patient-output-learnmore-container">
+            <button className="patient-output-learnmore"> yes</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
